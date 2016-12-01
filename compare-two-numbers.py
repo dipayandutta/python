8b24eb4d318725b33bb72583bd5d4ea5bc2e3e2a
Dipayan <inbox.dipayan@gmail.com>
2016-12-02 02:11:06 +0530

    Add files via upload

---
+++ compare-two-numbers.py
@@ -0,0 +1,24 @@
+'''
+Purpose :- To get more familier with if else logic
+Author :- Dipayan Dutta
+'''
+
+#read first number
+first_number = input("Enter First Number[integer only] ")
+#read second number
+second_number = input ('Enter second number [integer only] ')
+
+#now checking starts
+if (first_number == second_number):
+        print "Both Numbers are same"
+
+elif(first_number > second_number):
+    print "First Number is Greatr Than Second Number"
+
+else:
+    print "Second number is Greate Than First Number"
+        
+
+
+
+    
