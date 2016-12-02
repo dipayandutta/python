12d735adbaccf84d2fcd455d71a32e65674a1fd0
Dipayan <inbox.dipayan@gmail.com>
2016-12-03 02:26:37 +0530

    Add files via upload

--- function.py
+++ function.py
@@ -23,10 +23,24 @@ def check(number,max_number):
     else:
         print "some Thing Else"
 
+#function defination inside another function
+#Here i have to just call the function_four to get the addition result
+        
+def function_four():
+    def addition(number1,number2):
+        print "Given Number First One",number1
+        print "Given Number Second One ",number2
+        add = number1 + number2
+        print "Addition Result ",add
+
+    number1 = input("Enter first number")
+    number2 = input("Enter Second number")
+    f_five = addition(number1,number2)
+    
 
 function_one()#calling function one
 function_two()#calling function two
 check(10,20)#calling check function with first cond
 check(21,20)#calling check function with second cond
 check(20,20)#calling check function with third cond
-                   
+function_four() #calling function four                   
