a82821df1c2a438f81bfe808859cd1131d462349
Dipayan <inbox.dipayan@gmail.com>
2016-12-18 02:48:29 +0530

    using JSON api get the lat and long of a location
    I used google map api for searching the lat long of a location .

--- get_lat_long.py
+++ get_lat_long.py
@@ -3,13 +3,15 @@
   
    Author :- Dipayan Dutta
 '''
-
 import urllib2
 import json
 
 def main():
 	#Taking the city name as user input 
 	city_name= raw_input("Enter Ciry Name ==>  ")
+	#striping any white spaces from the input
+	city_name =''.join(city_name.split()) 
+	#print city_name
 	#creating the URL with appending the city name
 	urlData = "http://maps.googleapis.com/maps/api/geocode/json?address={}".format(city_name)
 	#print urlData
@@ -26,11 +28,16 @@ def main():
 #my getlatlong() function for extracting information 
 
 def getlatlong(data,city_name):
+	#loading the JSON data
 	information = json.loads(data)
+	#Using a counter to count total number of search results
+	total_cities = 0 
 	for info in information["results"]:
+		total_cities += 1
 		print info["formatted_address"]
 		print str(city_name)+"'s lat and long is {}".format(info["geometry"]["location"])
-
+	print "Total Search Result for {} is/are {}".format(city_name,total_cities)
+	
 if __name__ == '__main__':
 	main()
 
